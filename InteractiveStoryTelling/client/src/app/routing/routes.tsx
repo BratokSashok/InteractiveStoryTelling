bc37244a15f89717dd9f1e1dbfa02fbf446c6cb2
MaLoY6223 <mloy6223@gmail.com>
2025-05-13 19:24:16 +0300

    Что-то поменялось

--- InteractiveStoryTelling/client/src/app/routing/routes.tsx
+++ InteractiveStoryTelling/client/src/app/routing/routes.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { createBrowserRouter } from "react-router-dom";
 import { App } from "@app/App";
 import { HomePage } from "@pages/HomePage/HomePage";
@@ -34,3 +35,36 @@ export const router = createBrowserRouter([
       ],
     },
   ]);
+=======
+import { createBrowserRouter } from 'react-router-dom';
+import { App } from '../App';
+import { HomePage } from '@pages/HomePage/HomePage';
+import { LoginPage } from '@pages/login/ui/LoginPage';
+import RegistrationPage from '@pages/registration/ui/Registration';
+import { StoriesPage } from '@pages/StoriesPage/StoriesPage';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, 
+    children: [
+      {
+        index: true,
+        element: <HomePage />, // главная
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,// логин
+      },
+      {
+        path: 'registration',
+        element: <RegistrationPage />,// регистрация
+      },
+      {
+        path: "stories",
+        element: <StoriesPage />,
+      }
+    ],
+  },
+]);
+>>>>>>> Stashed changes
