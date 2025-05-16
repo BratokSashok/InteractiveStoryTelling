705dfe07ed118299566db1234baeb57802c31b36
MaLoY6223 <mloy6223@gmail.com>
2025-05-16 17:49:58 +0300

    Finnaly fixed all issues, that was before that moment

--- InteractiveStoryTelling/client/src/app/routing/routes.tsx
+++ InteractiveStoryTelling/client/src/app/routing/routes.tsx
@@ -1,31 +1,37 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { App } from '../App';
-import { HomePage } from '@pages/HomePage/HomePage';
-import { LoginPage } from '@pages/login/ui/LoginPage';
-import RegistrationPage from '@pages/registration/ui/Registration';
-import { StoriesPage } from '@pages/StoriesPage/StoriesPage';
+import { createBrowserRouter } from "react-router-dom";
+import { App } from "@app/App";
+// import { HomePage } from "@pages/home/HomePage";
+import { HomePage } from "@pages/HomePage/HomePage";
+import { LoginPage } from "@pages/login/ui/LoginPage";
+import { NotFoundPage } from "@pages/notFound/NotFoundPage";
+import { RegistrationForm } from "@features/auth/ui/registrationForm";
+import { StoriesPage } from "@pages/StoriesPage/StoriesPage";
 
 export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />, 
-    children: [
-      {
-        index: true,
-        element: <HomePage />, // главная
-      },
-      {
-        path: 'login',
-        element: <LoginPage />,// логин
-      },
-      {
-        path: 'registration',
-        element: <RegistrationPage />,// регистрация
-      },
-      {
-        path: "stories",
-        element: <StoriesPage />,
-      }
-    ],
-  },
-]);
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: 'login',
+          element: <LoginPage />,
+        },
+        {
+          path: 'registration',
+          element: <RegistrationForm />,
+        },
+        {
+          path: 'stories',
+          element: <StoriesPage />,
+        },
+        {
+            path: '*',
+            element: <NotFoundPage />, //страница 404 
+        }
+      ],
+    },
+  ]);
